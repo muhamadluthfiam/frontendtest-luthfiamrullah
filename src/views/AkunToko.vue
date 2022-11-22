292602471274921ef4bd2d6ef89991427b222c3c
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-22 10:56:47 +0700

    create layout akun toko

--- src/views/AkunToko.vue
+++ src/views/AkunToko.vue
@@ -1,35 +1,48 @@
 <template>
-  <main>
-    <p class="py-2">Assalamualaikum</p>
-    <p class="py-2">
-      <a
-        href="https://github.com/sschoger/heroicons-ui"
-        class="text-black font-bold"
-        >Hero Icons</a
-      >
-      by
-      <a href="https://twitter.com/steveschoger" class="text-black font-bold"
-        >Steve Schoger</a
-      >
-    </p>
-    <p class="py-2">
-      Adaptation of
-      <a
-        href="https://codyhouse.co/demo/responsive-sidebar-navigation/index.html"
-        class="text-black font-bold"
-        >a CodyHouse Demo</a
-      >
-    </p>
-    <p class="py-2 text-sm">
-      Made by
-      <a href="https://webartisan.be" class="text-black font-bold"
-        >Simon Depelchin</a
-      >,
-      <a href="https://twitter.com/SimonDepelchin" class="text-black font-bold"
-        >@SimonDepelchin</a
-      >
-      on Twitter
-    </p>
+  <main class="mx-10">
+    <div class="flex justify-start">
+      <button class="rounded-[6px] w-[132px] h-[33px] text-white bg-[#50CD89] mt-5 float-left">
+        Tambah Outlet
+      </button>
+    </div>
+    <div class="2xl:columns-3 mt-5 lg:columns-2">
+      <div class="col-4 col-xl-4">
+        <div class="content">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="card">
+                <div
+                  class="d-flex justify-content-between align-items-center"
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6">
+              <div class="card">
+                <div
+                  class="d-flex justify-content-between align-items-center"
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6">
+              <div class="card">
+                <div
+                  class="d-flex justify-content-between align-items-center"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 lg:mt-5">
+      <div class="card">
+        <div class="d-flex justify-content-between align-items-center"></div>
+      </div>
+    </div>
   </main>
 </template>
 <script setup>
